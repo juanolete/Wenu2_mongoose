486909d1b15c4ec7ca3baa3acea007dc96537175
Juan Jose Millan <juanjose@wenuwork.cl>
2018-08-09 16:15:49 -0400

    no message

---
+++ AP/build/gen/mg_build_info.c
@@ -0,0 +1,4 @@
+/* Auto-generated, do not edit. */
+const char *mg_build_id = "20180801-211807/???";
+const char *mg_build_timestamp = "2018-08-01T21:18:07Z";
+const char *mg_build_version = "2018080121";
