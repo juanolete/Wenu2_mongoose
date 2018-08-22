f122c71133bdc62b821e0f40e5caccb520ffb623
Juan Jose Millan <juanjose@wenuwork.cl>
2018-08-22 18:00:05 -0300

    Avances en libreria

--- ADE7858/include/mgos_ade7858.h
+++ ADE7858/include/mgos_ade7858.h
@@ -41,17 +41,27 @@
 #define AVA_24 0xE519 //Instantaneous value of Phase A apparent power.
 #define BVA_24 0xE51A //Instantaneous value of Phase B apparent power.
 #define CVA_24 0xE51B //Instantaneous value of Phase C apparent power.
+#define CHECKSUM_32 0xE51F //Checksum verification.
+#define VNOM_24 0xE520
+#define PHSTATUS_16 0xE600 //Phase peak register
 #define ANGLE0_16 0xE601 //Delay between Phase A voltage and Phase C voltage.
 #define ANGLE1_16 0xE602 //Delay between Phase B voltage and Phase C voltage.
 #define ANGLE2_16 0xE603 //Delay between Phase A voltage and Phase B voltage.
+#define PERIOD_16 0xE607 //Network line period
+#define PHNOLOAD_16 0xE608 //Phase no load register
+#define LINECYC_16 0xE60C //Line cycle accumulation mode count
+#define ZXTOUT_16 0xE60D //Zero-crossing timeout count
 
 //For configuration
 #define STATUS0_32 0xE502 //Interrupt Status Register 0.
 #define STATUS1_32 0xE503 //Interrupt Status Register 1.
+#define OILVL_24 0xE507 //Overcurrent threshold.
+#define OVLVL_24 0xE508 //Overvoltage threshold.
+#define SAGLVL_24 0xE509 //Voltage SAG level threshold. 
 #define MASK0_32 0xE50A //Interrupt Enable Register 0.
 #define MASK1_32 0xE50A //Interrupt Enable Register 1.
 #define COMPMODE_16 0xE60E //Computation-mode register.
-#define GAIN_16 0xE60F //PGA gains at ADC inputs.
+#define Gain_16 0xE60F //PGA gains at ADC inputs.
 #define CFMODE_16 0xE610 //CFx configuration register.
 #define CF1DEN_16 0xE611 //CF1 denominator.
 #define CF2DEN_16 0xE612 //CF2 denominator.
@@ -63,7 +73,57 @@
 #define CONFIG_16 0xE618 //ADE7878 configuration register.
 #define MMODE_8 0xE700 //Measurement mode register.
 #define ACCMODE_8 0xE701 //Accumulation mode register.
+#define LCYCMODE_8 0xE702 //Line accumulation mode behavior
+#define PEAKCYC_8 0xE703 //Peak detection half line cycles
+#define SAGCYC_8 0xE704 //SAG detectition half line cycles
+#define CFCYC_8 0xE705
+#define HSDC_CFG_8 0xE706 //HSDC configuratio register
 #define CONFIG2_8 0xEC01 //Configuration register used during PSM1.
+
+//DSP Data Memory RAM registers
+#define AIGAIN_24 0x4380 //Phase A current gain adjust
+#define AVGAIN_24 0x4381 //Phase A voltage gain adjust
+#define BIGAIN_24 0x4382 //Phase B current gain adjust
+#define BVGAIN_24 0x4383 //Phase B voltage gain adjust
+#define CIGAIN_24 0x4384 //Phase C current gain adjust
+#define CVGAIN_24 0x4385 //Phase C voltage gain adjust
+#define NIGAIN_24 0x4386 //Neutral current gain adjust
+
+#define AIRMSOS_24 0x4387 //Phase A current rms offset
+#define AVRMSOS_24 0x4388 //Phase A voltage rms offset
+#define BIRMSOS_24 0x4389 //Phase B current rms offset
+#define BVRMSOS_24 0x438A //Phase B voltage rms offset
+#define CIRMSOS_24 0x438B //Phase C current rms offset
+#define CVRMSOS_24 0x438C //Phase C voltage rms offset
+
+#define AVAGAIN_24 0x438E //Phase A apparent power gain adjust
+#define BVAGAIN_24 0x438F //Phase B apparent power gain adjust
+#define CVAGAIN_24 0x4390 //Phase C apparent power gain adjust
+#define AWGAIN_24 0x4391 //Phase A total active power gain adjust
+#define AWATTOS_24 0x4392 //Phase A total active power offset adjust.
+#define BWGAIN_24 0x4393 //Phase B total active power gain adjust
+#define BWATTOS_24 0x4394 //Phase B total active power offset adjust.
+#define CWGAIN_24 0x4395 //Phase C total active power gain adjust
+#define CWATTOS_24 0x4396 //Phase C total active power offset adjust.
+#define AVARGAIN_24 0x4397 //Phase A total reactive power gain adjust
+#define AVAROS_24 0x4398 //Phase A total reactive power offset adjust
+#define BVARGAIN_24 0x4399 //Phase B total reactive power gain adjust
+#define BVAROS_24 0x439A //Phase B total reactive power offset adjust
+#define CVARGAIN_24 0x439B //Phase C total reactive power gain adjust
+#define CVAROS_24 0x439C //Phase C total reactive power offset adjust
+#define AFWGAIN_24 0x439D //Phase A fundamental active power gain adjust. 
+#define AFWATTOS_24 0x439E //Phase A fundamental active power offset adjust.
+#define VATHR1_24 0x43A9
+#define VATHR0_24 0x43AA
+#define WTHR1_24 0x43AB
+#define WTHR0_24 0x43AC
+#define VARTHR1_24 0x43AD
+#define VARTHR0_24 0x43AE
+#define VANOLOAD_24 0x43B0 //No load threshold in the apparent power datapath.
+#define APNOLOAD_24 0x43B1 //No load threshold in the total/fundamental active power datapath. 
+#define DICOEFF_24 0x43B5 
+#define HPFDIS_24 0x43B6
+#define Run_16 0xE228
 
 /*
 struct mgos_ade7858_event_data{
--- ADE7858/src/mgos_ade7858.c
+++ ADE7858/src/mgos_ade7858.c
@@ -20,7 +20,63 @@ static void ade7858_irq(){
 */
 
 //read register
-static uint32_t ade7858_spi_read_register(uint16_t reg) {
+static uint8_t ade7858_spi_read_register_8(uint16_t reg) {
+  struct mgos_spi *spi = mgos_spi_get_global();
+  if (!spi) {
+    LOG(LL_ERROR,("Cannot get global SPI bus"));
+    return 0;
+  }
+
+  uint8_t reg0 = 0xFF & reg;
+  uint8_t reg1 =  0xFF & (reg >> 8);
+  uint8_t tx_data[3] = {read, reg1, reg0};
+  uint32_t rx_data;
+
+  struct mgos_spi_txn txn = {
+    .cs = mgos_sys_config_get_ade7858_cs_index(),
+    .mode = 3,
+    .freq = SPI_freq,
+  };
+  txn.hd.tx_len    = 3;
+  txn.hd.tx_data   = &tx_data;
+  txn.hd.dummy_len = 0;
+  txn.hd.rx_len    = 1;
+  txn.hd.rx_data   = &rx_data;
+  if(!mgos_spi_run_txn(spi, false, &txn)) {
+    LOG(LL_ERROR, ("SPI transaction failed"));
+    return 0;
+  }
+  return rx_data;
+}
+static uint16_t ade7858_spi_read_register_16(uint16_t reg) {
+  struct mgos_spi *spi = mgos_spi_get_global();
+  if (!spi) {
+    LOG(LL_ERROR,("Cannot get global SPI bus"));
+    return 0;
+  }
+
+  uint8_t reg0 = 0xFF & reg;
+  uint8_t reg1 =  0xFF & (reg >> 8);
+  uint8_t tx_data[3] = {read, reg1, reg0};
+  uint32_t rx_data;
+
+  struct mgos_spi_txn txn = {
+    .cs = mgos_sys_config_get_ade7858_cs_index(),
+    .mode = 3,
+    .freq = SPI_freq,
+  };
+  txn.hd.tx_len    = 3;
+  txn.hd.tx_data   = &tx_data;
+  txn.hd.dummy_len = 0;
+  txn.hd.rx_len    = 2;
+  txn.hd.rx_data   = &rx_data;
+  if(!mgos_spi_run_txn(spi, false, &txn)) {
+    LOG(LL_ERROR, ("SPI transaction failed"));
+    return 0;
+  }
+  return rx_data;
+}
+static uint32_t ade7858_spi_read_register_32(uint16_t reg) {
   struct mgos_spi *spi = mgos_spi_get_global();
   if (!spi) {
     LOG(LL_ERROR,("Cannot get global SPI bus"));
@@ -48,8 +104,64 @@ static uint32_t ade7858_spi_read_register(uint16_t reg) {
   }
   return rx_data;
 }
+
 //write register
-static void ade7858_spi_write_register(uint16_t reg, uint32_t val){
+static void ade7858_spi_write_register_8(uint16_t reg, uint8_t val){
+  struct mgos_spi *spi = mgos_spi_get_global();
+
+  if (!spi) {
+    LOG(LL_ERROR, ("Cannot get global SPI bus"));
+    return;
+  }
+
+  uint8_t reg0 = 0xFF & reg;
+  uint8_t reg1 = 0xFF & (reg >> 8);  
+
+  uint8_t tx_data[7] = {write, reg1, reg0, val};
+
+  struct mgos_spi_txn txn = {
+    .cs   = mgos_sys_config_get_ade7858_cs_index(),
+    .mode = 3,
+    .freq = SPI_freq,
+  };
+  txn.hd.tx_data   = &tx_data;
+  txn.hd.tx_len    = 4;
+  txn.hd.dummy_len = 0;
+  txn.hd.rx_len    = 0;
+  if (!mgos_spi_run_txn(spi, false, &txn)) {
+    LOG(LL_ERROR, ("SPI transaction failed"));
+    return;
+  }
+}
+static void ade7858_spi_write_register_16(uint16_t reg, uint16_t val){
+  struct mgos_spi *spi = mgos_spi_get_global();
+
+  if (!spi) {
+    LOG(LL_ERROR, ("Cannot get global SPI bus"));
+    return;
+  }
+
+  uint8_t reg0 = 0xFF & reg;
+  uint8_t reg1 = 0xFF & (reg >> 8);  
+  uint8_t val0 = 0xFF & val;
+  uint8_t val1 = 0xFF & (val >> 8);
+  uint8_t tx_data[7] = {write, reg1, reg0, val1, val0};
+
+  struct mgos_spi_txn txn = {
+    .cs   = mgos_sys_config_get_ade7858_cs_index(),
+    .mode = 3,
+    .freq = SPI_freq,
+  };
+  txn.hd.tx_data   = &tx_data;
+  txn.hd.tx_len    = 5;
+  txn.hd.dummy_len = 0;
+  txn.hd.rx_len    = 0;
+  if (!mgos_spi_run_txn(spi, false, &txn)) {
+    LOG(LL_ERROR, ("SPI transaction failed"));
+    return;
+  }
+}
+static void ade7858_spi_write_register_32(uint16_t reg, uint32_t val){
   struct mgos_spi *spi = mgos_spi_get_global();
 
   if (!spi) {
@@ -79,49 +191,78 @@ static void ade7858_spi_write_register(uint16_t reg, uint32_t val){
     return;
   }
 }
-
 static void ade7858_spi_write_init(void){
-  struct mgos_spi *spi = mgos_spi_get_global();
-  if (!spi) {
-    LOG(LL_ERROR, ("Cannot get global SPI bus"));
-    return;
-  }
-  struct mgos_spi_txn txn = {
-    .cs = mgos_sys_config_get_ade7858_cs_index(),
-    .mode = 3,
-    .freq = SPI_freq,
-  }
-  uint16_t reg = 0xEBFF;
-  uint8_t reg0 = 0xFF & reg;
-  uint8_t reg1 = 0xFF & (reg >> 8);
-  uint8_t val = 0xF;
-
-  uint8_t tx_data[4] = {write, reg1, reg0, val};
-
-  txn.hd.tx_data = &tx_data;
-  txn.hd.tx_len = 1;
-  txn.hd.dummy_len = 0;
-  txn.hd.rx_len    = 0;
-    if (!mgos_spi_run_txn(spi, false, &txn)) {
-    LOG(LL_ERROR, ("SPI transaction failed"));
-    return;
-  }
+  ade7858_spi_write_register_8(0xEBFF, 0xFF);
+  mgos_msleep(100);
+  ade7858_spi_write_register_8(0xEBFF, 0xFF);
+  mgos_msleep(100);
+  ade7858_spi_write_register_8(0xEBFF, 0xFF);
+  mgos_msleep(100);
 }
 
 //init
 bool mgos_ade7858_init(void){
   mgos_gpio_set_mode(mgos_sys_config_get_ade7858_rst_pin(), MGOS_GPIO_MODE_OUTPUT);
-  /*
   mgos_gpio_set_mode(mgos_sys_config_get_ade7858_irq_pin(), MGOS_GPIO_MODE_INPUT);
   mgos_gpio_set_pull(mgos_sys_config_get_ade7858_irq_pin(), MGOS_GPIO_PULL_UP);
+  /*
   mgos_gpio_set_int_handler(mgos_sys_config_get_ade7858_irq_pin()), MGOS_GPIO_INT_EDGE_NEG, ade7858_irq, NULL);
   mgos_gpio_enable_int(mgos_sys_config_get_ade7858_irq_pin());
   */
   ade7858_spi_write_init();
-  mgos_msleep(100);
-  ade7858_spi_write_init();
-  mgos_msleep(100);
-  ade7858_spi_write_init();
+  ade7858_spi_write_register_8(CONFIG2_8, 0xF0);
+  while (mgos_gpio_read(mgos_sys_config_get_ade7858_irq_pin())); //wait for !IRQ1 pin to set LOW
+  ade7858_spi_write_register_32(STATUS1_32, 0x00000000);
+  ade7858_spi_write_register_32(STATUS0_32, 0x00200000);
+  
+  // FInalmente, iniciar todos los registros
+  // ver DIgital Signal Processor section en datasheet
+  //Iniciar Gain registers
+  ade7858_spi_write_register_32(AIGAIN_24, 0x00000001);
+  ade7858_spi_write_register_32(BIGAIN_24, 0x00000001);
+  ade7858_spi_write_register_32(CIGAIN_24, 0x00000001);
+  ade7858_spi_write_register_32(AVGAIN_24, 0x00000001);
+  ade7858_spi_write_register_32(BVGAIN_24, 0x00000001);
+  ade7858_spi_write_register_32(CVGAIN_24, 0x00000001);
+  ade7858_spi_read_register_16(Gain_16, 0x0000);
+  ade7858_spi_read_register_32(AVAGAIN_24, 0x00000001);
+  ade7858_spi_read_register_32(BVAGAIN_24, 0x00000001);
+  ade7858_spi_read_register_32(CVAGAIN_24, 0x00000001);
+  ade7858_spi_read_register_32(AWGAIN_24, 0x00000001);
+  ade7858_spi_read_register_32(BWGAIN_24, 0x00000001);
+  ade7858_spi_read_register_32(CWGAIN_24, 0x00000001);
+  ade7858_spi_read_register_32(AVARGAIN_24, 0x00000001);
+  ade7858_spi_read_register_32(BVARGAIN_24, 0x00000001);
+  ade7858_spi_read_register_32(CVARGAIN_24, 0x00000001);
+  ade7858_spi_read_register_32(AFWGAIN_24, 0x00000001);
+  //Iniciar WTHR0 y WTHR1
+  ade7858_spi_read_register_32(AFWGAIN_24, 0x00000001);
+  ade7858_spi_read_register_32(AFWGAIN_24, 0x00000001);
+
+  //DSP run = 1
+  ade7858_spi_write_register_16(Run_16, 0x0001);
+  //Iniciate all RAM registers
+
+  //Iniciar AIGAIN, BIGAIN, CIGAIN, NIGAIN
+  //Start DSP setting run = 1
+  //Iniciar all  RAM registers. 3 veces
+  //Iniciar todos los registros menos CFMODE
+  //Leer todos los registros
+  //xWATTHR, xFWATTHR, xVARHR, xFVARFR y xVAHR
+  //Clear bit CF1DIS, CF2DIS y CF3DIS in CFMODE
+  //escribir 0xAD to 8bit 0xE7FE register
+  //escribir 0x80 to 8bit 0xE7E3
+  //leer todos los datos de RAM register
+  //asegurarse de que fueron bien escritos
+  //si alguno no fue bien escrito:
+  //    escribir 0xAD to 8bit 0xE7FE register
+  //    escribir 0x00 to 8bit 0xE7E3 register
+  //    reinicializar los registros
+  //    escribir los registros 3 veces
+  //    volver a habilitar write proteccion:
+  //    escribir 0xAD to 8bit 0xE7FE
+  //    escribir 0x80 to 8bit 0xE7E3
+  
 
   //CONFIGURAR
 
